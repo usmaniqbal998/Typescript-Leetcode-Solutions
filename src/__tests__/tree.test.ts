c655a8cdc68ea16a8f8b320e4cd82e3b0a02ead6
usmaniqbal998 <usman.iqbal998@gmail.com>
2022-11-17 08:23:48 +0100

    feat: Binary tree with traversal functions added

---
+++ src/__tests__/tree.test.ts
@@ -0,0 +1,25 @@
+import BinaryTree, {
+	inOrderTraversal,
+	Node,
+	postOrderTraversal,
+	preOrderTraversal,
+} from '../tree';
+
+it('inserts values inside a tree', () => {
+	const values = [2, 6, 8, 3, 9, 1, 10, 0, 20];
+
+	const btree = new BinaryTree({ value: 5, children: [] });
+	values.forEach((element) => {
+		btree.insertion(element, btree.root);
+	});
+
+	const preRes = preOrderTraversal(btree.root as Node<number>, []);
+
+	const inRes = inOrderTraversal(btree.root as Node<number>, []);
+
+	const postRes = postOrderTraversal(btree.root as Node<number>, []);
+
+	expect(preRes).toEqual([5, 2, 1, 0, 3, 6, 8, 9, 10, 20]);
+	expect(inRes).toEqual([0, 1, 2, 3, 5, 6, 8, 9, 10, 20]);
+	expect(postRes).toEqual([0, 1, 3, 2, 20, 10, 9, 8, 6, 5]);
+});
---
+++ src/tree.ts
@@ -0,0 +1,64 @@
+export interface Node<T> {
+	value: T;
+	children: Node<T>[];
+}
+
+export default class BinaryTree<T> {
+	root: Node<T>;
+
+	constructor(root: Node<T>) {
+		this.root = root;
+	}
+
+	insertion(value: T, current: Node<T>) {
+		if (this.root === undefined) {
+			this.root = { value: value, children: [] };
+			return;
+		}
+
+		if (current === undefined) {
+			return;
+		}
+
+		if (value < current.value) {
+			if (current.children[0] !== undefined)
+				this.insertion(value, current.children[0]);
+			else current.children[0] = { value, children: [] };
+		}
+		if (value > current.value) {
+			if (current.children[1] !== undefined)
+				this.insertion(value, current.children[1]);
+			else current.children[1] = { value, children: [] };
+		}
+	}
+}
+
+export function preOrderTraversal<T>(current: Node<T>, result: T[]): T[] {
+	if (current === undefined) return result;
+
+	result.push(current.value);
+	preOrderTraversal(current.children[0], result);
+	preOrderTraversal(current.children[1], result);
+
+	return result;
+}
+
+export function inOrderTraversal<T>(current: Node<T>, result: T[]): T[] {
+	if (current === undefined) return result;
+
+	inOrderTraversal(current.children[0], result);
+	result.push(current.value);
+	inOrderTraversal(current.children[1], result);
+
+	return result;
+}
+
+export function postOrderTraversal<T>(current: Node<T>, result: T[]): T[] {
+	if (current === undefined) return result;
+
+	postOrderTraversal(current.children[0], result);
+	postOrderTraversal(current.children[1], result);
+	result.push(current.value);
+
+	return result;
+}
