08e54cbf53948e3bdab4ff052729224f55f32f68
usmaniqbal998 <usman.iqbal998@gmail.com>
2022-11-24 08:29:15 +0100

    fix: easier solution for mergesortedlist

--- src/mergesortedlists.ts
+++ src/mergesortedlists.ts
@@ -9,18 +9,36 @@ export default class ListNode {
 }
 
 export function mergeTwoLists(
-	l2: ListNode | null,
-	l1: ListNode | null
+	list1: ListNode | null,
+	list2: ListNode | null
 ): ListNode | null {
-	if (l1 == null) {
-		return l2;
-	} else if (l2 == null) {
-		return l1;
-	} else if (l1.val < l2.val) {
-		l1.next = mergeTwoLists(l1.next, l2);
-		return l1;
-	} else {
-		l2.next = mergeTwoLists(l1, l2.next);
-		return l2;
+	const res = combineSorted(list1, list2);
+	return res;
+}
+
+function combineSorted(
+	list1: ListNode | null,
+	list2: ListNode | null
+): ListNode | null {
+	let res: ListNode | null = null;
+	if (list1 === null && list2 === null) {
+		return res;
 	}
+
+	if (list1 === null) {
+		return list2;
+	}
+
+	if (list2 === null) {
+		return list1;
+	}
+	if (list1!.val <= list2!.val) {
+		res = { val: list1!.val, next: null };
+		list1 = list1?.next || null;
+	} else {
+		res = { val: list2!.val, next: null };
+		list2 = list2?.next || null;
+	}
+	res.next = combineSorted(list1, list2);
+	return res;
 }
